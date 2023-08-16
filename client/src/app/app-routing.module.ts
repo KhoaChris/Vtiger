4d9b225e18068941f1734963be897c14d5e9799f
Bui Duc Dang Khoa <bddkhoa@gmail.com>
2023-08-16 23:15:44 +0700

    add about us page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'landing-page', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) }];
+const routes: Routes = [{ path: 'landing-page', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) }, { path: 'about-us', loadChildren: () => import('./about-us-page/about-us-page.module').then(m => m.AboutUsPageModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
